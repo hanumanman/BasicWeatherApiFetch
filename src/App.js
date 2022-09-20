70ad41e2b65186c625e890fc021b821528ae7a4c
Itztla <hanumanman37@gmail.com>
2022-09-20 18:05:13 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from "react";
+import Header from './components/Header';
+
+import AddTaskForm from './components/AddTaskForm';
+import TaskList from './components/TaskList';
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {id:"task1", title : "Learn JS", status: 0},
+    {id:"task2", title : "Code a todo list", status: 1},
+  ]);
+
+  const [showIncomplete, setShowIncomplete] = useState(false);
+  const [newTask, setNewTask] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTask) {
+      const task = {
+        id: Date.now(),
+        title: newTask,
+        status: 0,
+      }
+      setTasks([...tasks, task]);
+      setNewTask("");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  }
+
+  const setTaskStatus = (taskId, status) => {
+    setTasks (tasks.map(task => {
+      if (task.id === taskId) {
+        return {...task, status: status ? 1 : 0};
+      }
+      return task;
+    })
+    )
+  };
+
+  const removeTask = (taskId) => {
+    setTasks(tasks.filter(task => task.id !== taskId))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+<div className="container">
+    <Header title={"Todo List"} subtitle="Get things done (or not)"/>
+    <TaskList tasks={tasks} showIncomplete={showIncomplete} setTaskStatus={setTaskStatus} removeTask={removeTask} setShowIncomplete={setShowIncomplete}/>
+    <AddTaskForm handleSubmit={handleSubmit} handleInputChange={handleInputChange} newTask={newTask} />
+    
+</div>
+
+  )
 }
 
 export default App;
